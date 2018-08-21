d1db1a2f05bf82668f4c3b4a37d9b37aaf887f52
master <51758018@qq.com>
2018-08-21 20:29:16 +0800

    rename the program

--- ORMDemo/Models/Person.cs
+++ ORMDemo/Models/Person.cs
@@ -8,5 +8,9 @@ namespace Models
 {
     public class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Email { get; set; }
     }
 }
--- ORMDemo/ORMDemo/Program.cs
+++ ORMDemo/ORMDemo/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ORMDemo
+namespace OORM
 {
+    using Models;
+    using System.Configuration;
+    using System.Reflection;
+
     class Program
     {
         static void Main(string[] args)
         {
+            //利用反射创建对象
+            var nameSpace = ConfigurationManager.AppSettings["nameSpace"];
+            var className = ConfigurationManager.AppSettings["className"];
+            var classFullName = string.Format($"{nameSpace}.{className}");
+
+            var fullAss = @"C:\MyDemo\Mirror\ORMDemo\Models\bin\Debug\" + nameSpace + ".dll";
+
+            Assembly assembly = Assembly.LoadFile(fullAss); // 加载程序集（EXE 或 DLL） 
+            var t = assembly.GetType(classFullName);
+            var obj = Activator.CreateInstance(t);
+
+
+
+
+
+            //赋值
+
+            //传入ORM方法,反射出对象的属性,基于约定ID为主字段 
+
+            Console.ReadLine();
         }
     }
 }
