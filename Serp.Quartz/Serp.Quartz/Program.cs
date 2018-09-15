9569b2269427b1ef5fac90bd77fc2d48e031eefd
oufu99 <111>
2018-09-15 18:23:22 +0800

    添加Quartz调度服务

---
+++ Serp.Quartz/Serp.Quartz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+
+namespace Serp.Quartz
+{
+    class Program
+    {
+        public static IScheduler CurrentSched { get; private set; }
+
+        static void Main(string[] args)
+        {
+            CurrentSched = StdSchedulerFactory.GetDefaultScheduler();   //从工厂中获取一个调度器实例化
+
+            JobWithSimpleTriggerDemo();
+
+            CurrentSched.Start();  //开启调度器
+            Console.WriteLine("开始调度");
+            Console.ReadLine();
+        }
+        private static void JobWithSimpleTriggerDemo()
+        {
+            IJobDetail simpleJob = JobBuilder.Create<SimpleJob>().WithIdentity("renwu1", "renwuGroup").Build();    //创建一个任务
+            ITrigger simpleTrigger = TriggerBuilder.Create().WithIdentity("chufa1", "chufaGroup").StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever()).Build();   //创建一个简单触发器，每隔5秒执行一次
+            CurrentSched.ScheduleJob(simpleJob, simpleTrigger);    //把任务、简单触发器加入调度器
+        }
+    }
+}
---
+++ Serp.Quartz/Serp.Quartz/SimpleJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Serp.Quartz
+{
+    /// <summary>
+    /// 简单任务
+    /// </summary>
+    public class SimpleJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(string.Format("[{0}] 任务处理中...", DateTime.Now.ToString("HH:mm:ss")));
+            //业务逻辑处理
+            Console.WriteLine("逻辑处理中");
+             string json=  ConfigurationManager.AppSettings["JsonData"];
+        }
+    }
+}
