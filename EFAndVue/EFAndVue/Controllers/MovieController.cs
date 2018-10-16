213d6e4b919b692d14279ad8d7153981e3a67329
oufu99 <51758018@qq.com>
2018-10-16 17:37:43 +0800

    添加 zui

--- EFAndVue/EFAndVue/Controllers/MovieController.cs
+++ EFAndVue/EFAndVue/Controllers/MovieController.cs
@@ -30,11 +30,30 @@ namespace EFAndVue.Controllers
         }
         public string LoadMovies(PageInfo page)
         {
-            using (var db = new MovieDbContext())
-            {
-                var movies = db.Movies.Where(c => true).ToList();
-                return JsonConvert.SerializeObject(movies);
-            }
+            //using (var db = new MovieDbContext())
+            //{
+            //    var movies = db.Movies.Where(c => true).ToList();
+            //    var Data = new { Data = movies };
+            //    return JsonConvert.SerializeObject(Data);
+            //}
+
+
+
+            //    "pager": {
+            //        "page": 1,           // 当前数据对应的页码
+            //"recTotal": 1001,    // 总的数据数目
+            //"recPerPage": 10,    // 每页数据数目
+
+            var pageInfo = new { page = 1, recTotal = 1001, recPerPage = 10 };
+
+
+            var data = new List<dynamic>();
+            data.Add(new { time = "00:11:12", hero = "幻影刺客", action = "击杀", target = "斧王", desc = "幻影刺客击杀了斧王。" });
+            data.Add(new { time = "00:11:14", hero = "幻影刺客1", action = "击杀", target = "斧王", desc = "幻影刺客bingo了死骑。" });
+            data.Add(new { time = "00:11:12", hero = "幻影刺客2", action = "击杀", target = "斧王", desc = "幻影刺客击杀了斧王。" });
+            data.Add(new { time = "00:11:12", hero = "幻影刺客3", action = "击杀", target = "斧王", desc = "幻影刺客击杀了斧王。" });
+            var result = new { data = data, result = "success", message = "", pager = pageInfo };
+            return JsonConvert.SerializeObject(result);
 
         }
 
