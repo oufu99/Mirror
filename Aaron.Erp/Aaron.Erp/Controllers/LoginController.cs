7e6a4c53eda64c54cbc9e7abaf28862df390de50
oufu99 <51758018@qq.com>
2019-04-24 14:17:03 +0800

    整理项目

---
+++ Aaron.Erp/Aaron.Erp/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aaron.Erp.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aaron.Erp/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class BaseModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int ManuId { get; set; }
+
+    }
+}
