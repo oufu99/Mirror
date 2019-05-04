8cfa8e6b7595ee58ac773c4c8ab6cf05e999655c
打野的小妞 <oufu99@126.com>
2019-05-04 19:42:55 +0800

    修改log4

--- Aaron.Common/Aaron.Common/Log4Helper.cs
+++ Aaron.Common/Aaron.Common/Log4Helper.cs
@@ -7,25 +7,22 @@ using System.Threading.Tasks;
 
 namespace Aaron.Common
 {
-    public static class Log4Helper
+    public class Log4Helper
     {
-        static ILog logger = log4net.LogManager.GetLogger("errLog4");
-        static ILog log = log4net.LogManager.GetLogger("infoLog4");
+
         public static void WriteInfo(string msg)
         {
             //记录信息
-            log.Info(msg);
+            ILog infoLogger = log4net.LogManager.GetLogger("RollingLogFileAppender");
+            infoLogger.Info(msg);
         }
         public static void WriteErr(string msg)
         {
             //记录错误
-            logger.Error(msg);
+            ILog errorLogger = log4net.LogManager.GetLogger("RollingLogFileAppender2");
+            errorLogger.Error(msg);
         }
 
-        public static void WriteFatal(string msg)
-        {
-            //记录严重错误
-            logger.Fatal(msg);
-        }
+
     }
 }
--- Aaron.Erp/Aaron.Erp/Controllers/HomeController.cs
+++ Aaron.Erp/Aaron.Erp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Aaron.Erp.App_Start.Filter;
+﻿using Aaron.Common;
+using Aaron.Erp.App_Start.Filter;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,9 @@ namespace Aaron.Erp.Controllers
         [IgnoreFilter]
         public ActionResult Index(BaseModel model)
         {
+
+
+            Log4Helper.WriteErr("错误执行中...");
             return View();
         }
     }
