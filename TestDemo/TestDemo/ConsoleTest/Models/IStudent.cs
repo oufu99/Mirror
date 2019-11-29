0167c89b8484afe6f61fdeeaa64f18b656fc142f
oujialin <oujialin@dossen.com>
2019-11-29 17:33:48 +0800

    Aaron的程序自动批量提交

---
+++ TestDemo/TestDemo/ConsoleTest/Models/IStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.Models
+{
+    public interface IStudent
+    {
+        void Say();
+    }
+
+
+
+    public class Student : IStudent
+    {
+        public void Say()
+        {
+            Console.WriteLine("我是Student");
+        }
+    }
+}
--- TestDemo/TestDemo/ConsoleTest/Program.cs
+++ TestDemo/TestDemo/ConsoleTest/Program.cs
@@ -1,7 +1,12 @@
-﻿using CommonServiceLocator;
+﻿
+using ConsoleTest.Models;
 using IModels;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -13,34 +18,37 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            IServiceLocator locator = new MockServiceLocator(new object[] { new Student(),
-                                             new NullReferenceException() });
+            //IServiceLocator locator = new MockServiceLocator(new object[] { new Student(),
+            //                                 new NullReferenceException() });
 
             //IStudent instance = locator.GetInstance<IStudent>();
             //instance.Say();
 
-            ServiceLocatorProvider provider = new ServiceLocatorProvider(() => locator);
+            //ServiceLocatorProvider provider = new ServiceLocatorProvider(() => locator);
 
-            ServiceLocator.SetLocatorProvider(provider);
-            var student = ServiceLocator.Current.GetInstance<IStudent>();
+            //ServiceLocator.SetLocatorProvider(provider);
+            //var student = ServiceLocator.Current.GetInstance<IStudent>();
+            //student.Say();
+
+
+            IUnityContainer container = new UnityContainer();
+
+            //container.Registrations
+
+            //-----------配置方式注入-----------
+            UnityConfigurationSection configuration = (UnityConfigurationSection)ConfigurationManager.GetSection(UnityConfigurationSection.SectionName);
+            configuration.Configure(container, "Default");
+
+            //-----------注入服务定位器-----------
+            UnityServiceLocator locator = new UnityServiceLocator(container);
+            ServiceLocator.SetLocatorProvider(() => locator);
+
+            IStudent student = locator.GetInstance<IStudent>();
             student.Say();
             Console.ReadLine();
         }
     }
 
-    public interface IStudent
-    {
-        void Say();
-    }
 
-
-
-    public class Student : IStudent
-    {
-        public void Say()
-        {
-            Console.WriteLine("我是Student");
-        }
-    }
 
 }
