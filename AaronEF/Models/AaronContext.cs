2d7a65696f4e4c723687b49dc233828d153a4236
打野的小妞 <oufu99@gmail.com>
2020-01-10 16:37:19 +0800

    aaron commit

---
+++ AaronEF/Models/AaronContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class AaronContext : DbContext
+    {
+        public AaronContext():base("name=connSqlServer")
+        {
+             
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
