49b95ec80bf2926be2549f6933829f18dd1d4e40
打野的小妞 <oufu99@gmail.com>
2020-02-21 20:11:06 +0800

    aaron commit

--- TestDemo/TestDemo/ConsoleTest/Program.cs
+++ TestDemo/TestDemo/ConsoleTest/Program.cs
@@ -28,19 +28,45 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("主线程1");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            var task = Asy();
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("主线程2");
 
-
-
-            WriteLineDelegate readLine = new WriteLineDelegate(Console.WriteLine);
-            readLine("Try!");
+            Console.WriteLine("主线程3");
 
             Console.ReadLine();
-
 
 
         }
 
 
+        static async Task<string> Asy()
+        {
+            Console.WriteLine("内部1");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.Run(() => {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(5000); });
+            Console.WriteLine("内部2");
+            await Task.Run(() => {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(5000); });
+            Console.WriteLine("内部3");
+
+            return "99";
+        }
+
+
+        static async Task<string> Asy2()
+        {
+            Console.WriteLine("内部11");
+
+            await Task.Run(() => { Thread.Sleep(5000); });
+            Console.WriteLine("内部22");
+            return "99";
+        }
 
 
     }
